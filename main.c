635f92185c9d4fff8cbbfae00ff01f3b4e28d05e
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-30 10:06:00 -0500

    Intecambiando valores

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int firstNumber;
+    int secondNumber;
+
+    printf("Ingresa el primer valor: ");
+    scanf("%i", &secondNumber);
+
+    printf("Ingresa el segundo valor: ");
+    scanf("%i", &firstNumber);
+
+    printf("El valor intercambiado del primer numero es: %i \n", firstNumber);
+    printf("El valor intercambiado del segundo numero es: %i", secondNumber);
+
+
+
+    return 0;
+}

150c79fe381b8c8dcf47b9721506e3613d7b1934
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-30 12:40:54 -0500

    Condicional switch

--- switch.c
+++ switch.c
@@ -5,23 +5,30 @@ int main()
 {
   int option;
 
-  printf("Ingresa un numero: ");
+  printf("Te encuentras en un sueño y tienes tres caminos \n");
+
+  printf("Escribe 1 si quieres ir por el camino de los dulces \n");
+  printf("Escribe 2 si quieres ir por el camino de madera \n");
+  printf("Escribe 3 si quieres ir por el camino de los gatitos \n");
+
+  printf("Escoge una opción \n");
   scanf("%i", &option);
 
   switch(option){
     case 1:
-    printf("Elegiste el numero 1 \n");
+    printf("Lo siento, moriste en el camino. \n");
     break;
     case 2:
-    printf("Elegiste el numero 2 \n");
+    printf("Te has perdido. \n");
     break;
     case 3:
-    printf("Elegiste el numero 3 \n");
+    printf("Los gatitos lindos te llevan a una puerta y al cruzar te despiertas! \n");
     break;
     default:
-    printf("Elegiste una opcion invalida \n");
+    printf("Losiento, esta no es una opcion correcta. \n");
     break;
   }
+
 
   return 0;
 }

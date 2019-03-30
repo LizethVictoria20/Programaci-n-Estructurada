8786887614de62cd8e5c3bd17f947e1d8427e552
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-30 10:36:02 -0500

    Sumandole el mismo valor de la variable

--- operadoresDeAsignacion.c
+++ operadoresDeAsignacion.c
@@ -3,45 +3,11 @@
 
 int main()
 {
+  int x = 10;
 
-  printf("Operadores de asignación \n");
+  x += x;
 
-  int value;
+  printf("El valor de x es: %i", x);
 
-  value = 11;
-  printf("El valor de la variable de value es: %i \n", value);
-
-  value += 1;
-  printf("El valor de la variable de value es: %i \n", value);
-
-  value -= 2;
-  printf("El valor de la variable de value es: %i \n", value);
-
-  value *= 3;
-  printf("El valor de la variable de value es: %i \n", value);
-
-  value /= 4;
-  printf("El valor de la variable de value es: %i \n", value);
-
-  value %= 5;
-  printf("El valor de la variable de value es: %i \n", value);
-
-
-  //Operadores de incremento y decremento
-
-  printf("Operadores de incremento y decremento \n");
-
-  int valor, rest;
-
-  valor = 20;
-  rest = valor++;
-
-  printf("El valor de value es: %i \n", rest);
-
-  valor = 20;
-  rest = valor--;
-
-  printf("El valor de value es: %i", valor);
-
-    return 0;
+  return 0;
 }

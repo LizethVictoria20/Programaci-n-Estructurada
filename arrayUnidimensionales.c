3dc99fc925a2f00b1d425f439f5dff9012b458d9
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-31 14:44:30 -0500

    Buscando el numero mayor de un arreglo

--- arrayUnidimensionales.c
+++ arrayUnidimensionales.c
@@ -3,12 +3,19 @@
 
 int main()
 {
-  int integerArray[10];
+  int integerArray[5];
+  int max = 0;
 
-  for(int i = 0; i < 10; i++){
-    integerArray[i] = i * i;
-      printf("El numero (%i): %i \n", i, integerArray[i]);
-  }
+
+  for(int i = 0; i < 6; i++){
+    printf("valor %i \n", i);
+    scanf("%i \n", &integerArray[i]);
+
+    if(integerArray[i] > max){
+      max = integerArray[i];
+    };
+  };
+  printf("El numero mayor es: %i \n", max);
 
   return 0;
 }
